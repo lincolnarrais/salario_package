44f2c27a798a7a845807617b28257473bb69884e
Lincoln Arrais <lincoln_arrais@hotmail.com>
2022-08-03 08:49:40 -0300

    Atualização de documentação

--- example/salario_package_example.dart
+++ example/salario_package_example.dart
@@ -7,3 +7,11 @@ void main(List<String> arguments) {
   final salario = Salario(salarioBruto: 16500);
   print(salario);
 }
+
+// Outputs:
+//
+// Salário bruto: R$ 16.500,00
+// Desconto INSS: R$ 828,39 (5,02%)
+// Desconto IRRF: R$ 3.440,33 (20,85%)
+// Desconto total: R$ 4.268,72 (25,87%)
+// Salário líquido: R$ 12.231,28
--- lib/src/i_salario_data.dart
+++ lib/src/i_salario_data.dart
@@ -2,31 +2,31 @@ abstract class ISalarioData {
   /// Salário Mínimo vigente.
   double get salarioMinimo;
 
-  // INSS
+  // ========== INSS ==========
 
   /// Teoricamente o menor salário tributável.
   /// Logo, corresponde ao [salarioMinimo].
   double get inssValorMaximoPatamar1;
+
   double get inssValorMaximoPatamar2;
+
   double get inssValorMaximoPatamar3;
 
-  /// Teto do INSS.
+  /// Teto do INSS, valor máximo levado em conta para tributação.
   double get inssValorMaximoPatamar4;
 
+  /// Menor alíquota INSS
   double get inssPercDescontoPatamar1;
+
   double get inssPercDescontoPatamar2;
 
-  /// Porcentagem para por quem atinge, mas não ultrapassa,
-  /// [inssValorMaximoPatamar3].
   double get inssPercDescontoPatamar3;
 
-  /// Porcentagem paga por quem atinge ou ultrapassa
-  /// [inssValorMaximoPatamar4], ou seja, o Teto do INSS.
   double get inssPercDescontoPatamar4;
 
-  // IRRF - Imposto de Renda Retido na Fonte
+  // ========== IRRF - Imposto de Renda Retido na Fonte =========
 
-  /// Valor (da Base de Cálculo IRRF) até qual se é isento de IRRF.
+  /// Valor (da Base de Cálculo IRRF) até o qual se é isento de IRRF.
   double get irrfValorMaximoIsento;
 
   /// Valor (da Base de Cálculo IRRF) até o qual se
@@ -39,15 +39,18 @@ abstract class ISalarioData {
 
   /// Valor (da Base de Cálculo IRRF) até o qual se
   /// paga o [irrfPercDescontoPatamar3].
-  /// Acima deste valor, paga-se o [irrfPercDescontoPatamar4].
+  /// Acima deste valor, paga-se o [irrfPercDescontoPatamar4],
+  /// sem limite máximo.
   double get irrfValorMaximoPatamar3;
 
   double get irrfPercDescontoPatamar1;
+
   double get irrfPercDescontoPatamar2;
+
   double get irrfPercDescontoPatamar3;
 
-  /// Porcentagem paga quando a Base de Cálculo do
-  /// IRRF ultrapassa [irrfPercDescontoPatamar3].
+  /// Alíquota IRRF paga quando a Base de Cálculo do
+  /// IRRF ultrapassa [irrfValorMaximoPatamar3].
   double get irrfPercDescontoPatamar4;
 
   /// Valor, por cada dependente, subtraído do cálculo base do IRRF.
